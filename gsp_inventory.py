1af56db9cbdc695caf891a21a42f386f8d862f11
Hassan Ahmed <ahmed.hassan.112.ha@gmail.com>
2020-11-11 20:49:27 +0500

    Completed work on gsp_inventory.py

--- gsp_inventory.py
+++ gsp_inventory.py
@@ -6,7 +6,7 @@ GSP Excel to CSV
 @contributor: Hassan Ahmed
 @contact: ahmed.hassan.112.ha@gmail.com
 @owner: Patrick Mahoney
-@version: 1.0.7
+@version: 1.1
 
 This module is created to Convert GSP Excel Inventory Feed file to TSV
     - To be run in the same directory the data file is located
@@ -15,27 +15,21 @@ TODO: All after this
 '''
 
 HELP_MESSAGE = '''Usage:
-    The script is capable of running without any argument provided. All behavorial
-    variables will be reset to default.
+    The script is capable of running without any argument provided. All behavorial variables will be reset to default.
 
-    $ python3 suredone_download.py [options]
+    $ python[3] gsp_inventory.py [options]
 
 Parameters/Options:
     -h  | --help            : View usage help and examples
-    -d  | --delimter        : Delimiter to be used as the separator in the CSV file saved by the script
-        |                       - Default is comma ','.
-    -f  | --file            : Path to the configuration file containing API keys
-        |                       - Default in %APPDATA%/local/suredone.yaml on Window
-        |                       - Default in $HOME/suredone.yaml
-    -o  | --output          : Path for the output file to be downloaded at
-        |                       - Default in %USERPROFILE%/Downloads/SureDone_Downloads_yyyy_mm_dd-hh-mm-ss.csv
-        |                       - Default in $HOME/downloads/SureDone_Downloads_yyyy_mm_dd-hh-mm-ss.csv
-    -p  | --preserve        : Do not delete older files that start with 'SureDone_' in the download directory
-        |                       - This funciton is limited to default download locations only.
-        |                       - Defining custom output path will render this feature useless.
+    -i  | --input           : Path to the input file
+        |                       - Linux: Defaults to $HOME/Downloads
+        |                       - Windows: Defaults to %USERPROFILE%\\downloads
+    -o  | --output          : Path to the output file
+        |                       - Linux: Defaults to $HOME/Downloads
+        |                       - Windows: Defaults to %USERPROFILE%\\downloads        
+    -d  | --delimter        : Single character to be used as delimiter for the tsv (default='\\t' (tab space))
+    -p  | --preserve        : Do not delete original file if declared
     -v  | --verbose         : Show outputs in terminal as well as log file
-    -w  | --wait            : Custom timeout for requests invoked by the script (specified in seconds)
-        |                       - Default: 15 seconds
 
 Example:
     $ python3 suredone_download.py
@@ -53,16 +47,31 @@ Example:
 from pathlib import Path
 import sys
 import inspect
+import time
 import csv
 import os
 import pandas as pd
-from sys import platform
+from datetime import datetime
+import traceback
+import getopt
+import shutil
 
-# Record python version for checking when the script kicks in
-PYTHON_VERSION = float(sys.version[:sys.version.index(' ')-2])
+currentMilliTime = lambda: int(round(time.time() * 1000))
+
+# Time tracking variables
+RUN_TIME = currentMilliTime()
+START_TIME = datetime.now()
+
+# Record python version and platform for checking when the script kicks in
+PYTHON_VERSION = float(sys.version[:sys.version.index(' ') - 2])
+if sys.platform == 'win32' or sys.platform == 'win64':  # Windows
+    PLATFORM = 'windows'
+elif sys.platform == 'linux' or sys.platform == 'linux2':  # Linux
+    PLATFORM = 'linux'
+
 
 def main(argv):
-    '''
+    """
     Main function that execute the whole functionality.
 
     Pipline:
@@ -70,25 +79,355 @@ def main(argv):
         - Check Python Version
         - Check operating system
         - Parse arguments
+        - Find defaults if input and output not provided
         - Verify:
             - File exists
             - File is not opened by someone
         - Read excel file
         - Fill na values by empty string or Int32Dtype (nullable type)
             - Maybe fill all of them with just an empty string?
+        - Save the file to .tsv
 
-
-    :param argv:
+    :param argv: arguments coming from the commandline
     :return:
-    '''
+    """
     localFrame = inspect.currentframe()
 
+    # Verify python version and platform type
+    checkPlatformAndPythonVersion()
+    LOGGER.writeLog("Platform type and python version verified.", localFrame.f_lineno)
 
+    # Parse arguments
+    inputFilePath, outputFilePath, delimiter, preserveOldFiles, verbose = parseArgs(argv)
+    LOGGER.writeLog("Args parsed...", localFrame.f_lineno)
+
+    # Read file
+    data = pd.read_excel(inputFilePath)
+    LOGGER.writeLog("File loaded...", localFrame.f_lineno)
+
+    # Fill nas with null values so they can be interpreted as null in SQL Server
+    data['Site'].fillna('', inplace=True)
+    data['ItemNumber'].fillna('', inplace=True)
+    data['QuantityOnHand'].fillna(0, inplace=True)
+
+    # Convert quantity in hand to integer
+    data['QuantityOnHand'] = data['QuantityOnHand'].astype(int)
+    LOGGER.writeLog("Data processed...", localFrame.f_lineno)
+
+    # Save file as tsv
+    # print (data)
+    data.to_csv(outputFilePath, sep='\t', index=False)
+    LOGGER.writeLog("File saved as .tsv ...", localFrame.f_lineno)
+
+    # Time to remove the original file (If preserve is declared as a command line flag)
+    if preserveOldFiles:
+        os.remove(inputFilePath)
+
+
+def parseArgs(argv):
+    """
+    Function that parses the arguments sent from the command line
+    and returns the behavioral variables to the caller.
+
+    :param argv: str: Arguments sent through the command line
+    :return:
+        inputPath: str: path to the directory where input file is present
+            - Linux: Defaults to $HOME/Downloads in
+            - Windows: Defaults to %USERPROFILE%\\downloads\\
+<br>    delimiter: str: Single character to be used as delimiter for the tsv (default=',')
+        #TODO: Since its a tsv, maybe tab as delimiter?
+<br>    outputPath: str: Output directory where output file is to be stored
+            - Linux: Defaults to $HOME/Downloads in
+            - Windows: Defaults to %USERPROFILE%\downloads\
+        preserve: boolean: Determines whether to remove all occurrences of the input file (default=False)
+        verbose: boolean: Show log outputs in the console
+    """
+    localFrame = inspect.currentframe()
+    # Defining options in for command line arguments
+    options = "hi:o:d:vp"
+    long_options = ["help", "input=", "output=", 'delimiter=', 'verbose', 'preserve']
+
+    # Note about validating if file is opened or not
+    #   - One of the files is an Excel 1997-compatiblity Mode xls which opens regardless of whether it is being used by
+    #   someone else
+    #   - Second file a csv which also gives no problem in reading if it is opened by someone else
+    # print(pd.read_csv(inputFilePath))
+
+    # This is the same for now. Maybe need to change separately for both OSes
+    if PLATFORM == 'windows':
+        inputDefaultPath = os.path.join(os.path.expanduser('~'), 'Downloads', 'InventoryList.xls')
+    elif PLATFORM == 'linux':
+        inputDefaultPath = os.path.join(os.path.expanduser('~'), 'Downloads', 'InventoryList.xls')
+
+    # This is the same for now. Maybe need to change separately for both OSes
+    if PLATFORM == 'windows':
+        outputDefaultPath = os.path.join(os.path.expanduser('~'), 'Downloads', 'gsp_inventory.tsv')
+    elif PLATFORM == 'linux':
+        outputDefaultPath = os.path.join(os.path.expanduser('~'), 'Downloads', 'gsp_inventory.tsv')
+
+    # Arguments
+    inputFilePath = inputDefaultPath
+    outputFilePath = outputDefaultPath
+    delimiter = ','
+    verbose = False
+    preserveOldFiles = False
+
+    # Extracting arguments
+    try:
+        opts, args = getopt.getopt(argv, options, long_options)
+    except getopt.GetoptError:
+        # Not logging here since this is a command-line feature and must be printed on console
+        print("Error in arguments!")
+        print(HELP_MESSAGE)
+        exit()
+
+    for option, value in opts:
+        if option == '-h':
+            # Turn on verbose, print help message, and exit
+            LOGGER.verbose = True
+            print(HELP_MESSAGE)
+            sys.exit()
+        elif option in ("-i", "--input"):
+            inputFilePath = value
+            if not os.path.exists(inputFilePath) or os.path.isdir(inputFilePath) or not inputFilePath.endswith(
+                    '.xls'):
+                LOGGER.writeLog(
+                    """Invalid file path. Check if it exists, is not a directory and has '.xls' extension.
+                     \rReverting to defaults.""",
+                    localFrame.f_lineno, severity='warning', data={'code': 1})
+                inputFilePath = inputDefaultPath
+        elif option in ("-o", "--output"):
+            outputFilePath = value
+            if not os.path.exists(os.path.dirname(outputFilePath)) or os.path.isdir(outputFilePath):
+                LOGGER.writeLog(
+                    """Invalid output file path. Check if it exists and is not a directory.
+                     \rReverting to defaults.""",
+                    localFrame.f_lineno, severity='warning', data={'code': 1})
+                outputFilePath = outputDefaultPath
+        elif option in ("-d", "--delimiter"):
+            delimiter = value
+            delimiter = validateDelimiter(delimiter)
+        elif option in ("-p", "--preserve"):
+            preserveOldFiles = True
+        elif option in ("-v", "--verbose"):
+            verbose = True
+
+    # Updating logger's behavior based on verbose
+    LOGGER.verbose = verbose
+
+    return inputFilePath, outputFilePath, delimiter, preserveOldFiles, verbose
+
+
+def validateDelimiter(delimiter):
+    """
+    Function that validates the delimiter option input by the user.
+    Main issues to check for is length and make sure that the chosen delimiter is within a list of acceptable options.
+    :param delimiter: str: The user-specified delimiter option
+    :return:
+<br>    delimiter : str: The same delimiter if validated and a ',' as a delimiter if not validated.
+    """
+    localFrame = inspect.currentframe()
+    # Account for '\\t' and '\t'
+    if delimiter == '\\t':
+        delimiter = '\t'
+
+    # Check for length
+    if len(delimiter) > 1:
+        LOGGER.writeLog("Length of the delimiter was greater than one character, switching to default ',' delimiter.",
+                        localFrame.f_lineno, severity='warning')
+        delimiter = ','
+        return delimiter
+
+    # Check that it's within acceptable options
+    acceptableDelimiters = [',', '\t', ':', '|', ' ']
+
+    if delimiter not in acceptableDelimiters:
+        LOGGER.writeLog("Delimiter was not selected from acceptable options, switching to ',' default delimiter.",
+                        localFrame.f_lineno, severity='warning')
+        delimiter = ','
+        return delimiter
+
+    return delimiter
+
+
+def checkPlatformAndPythonVersion():
+    """
+    Function that checks python version and platform
+    Will exit the code with an error entry in the log if requirements not satisfied
+    :return:
+    """
+    localFrame = inspect.currentframe()
+    # Check if python version is 3.5 or higher
+    """
+    # NOTE:
+    # More precision is not required since python is a very compatible and platform-free language (Windows Python 3.6 
+    # and Linux Python 3.8 can easily run the same file without any errors.
+    """
+    if not PYTHON_VERSION >= 3.5:
+        LOGGER.writeLog("Must use Python version 3.5 or higher!", localFrame.f_lineno, severity='code-breaker',
+                        data={'code': 1})
+        exit()
+
+    # Check if the platform is either windows or linux
+    if PLATFORM not in ('windows', 'linux'):
+        LOGGER.writeLog("Please use Windows or Linux platform.", localFrame.f_lineno, severity='code-breaker',
+                        data={'code': 1})
+
+
+""" Custom Exceptions that will be caught by the script """
+
+
+class Logger(object):
+    """ The logger class that will handle all outputs, may it be console or log file. """
+
+    def __init__(self, verbose=False):
+        self.terminal = sys.stdout
+        self.log = open(self.getLogPath(), "a")
+        # Write the header row
+        self.log.write(' Ind. |LineNo.| Time stamp  : Message')
+        self.log.write('\n=====================================\n')
+        self.verbose = verbose
+
+    def getLogPath(self):
+        """
+        Function that will determine the default log file path based on the operating system being used.
+        Will also create appropriate directories they aren't present.
+
+        Returns
+        -------
+            - logFile : fileIO
+                File IO for the whole script to log to.
+        """
+        # Define the file name for logging
+        temp = datetime.now().strftime('%Y_%m_%d-%H-%M-%S')
+        logFileName = "suredone_download_" + temp + ".log"
+
+        # If the platform is windows, set the log file path to the current user's Downloads/log folder
+        if sys.platform == 'win32' or sys.platform == 'win64':  # Windows
+            logFilePath = os.path.expandvars(r'%USERPROFILE%')
+            logFilePath = os.path.join(logFilePath, 'Downloads')
+            logFilePath = os.path.join(logFilePath, 'log')
+            if os.path.exists(logFilePath):
+                return os.path.join(logFilePath, logFileName)
+            else:  # Create the log directory
+                os.mkdir(logFilePath)
+                return os.path.join(logFilePath, logFileName)
+
+        # If Linux, set the download path to the $HOME/downloads folder
+        elif sys.platform == 'linux' or sys.platform == 'linux2':  # Linux
+            logFilePath = os.path.expanduser('~')
+            logFilePath = os.path.join(logFilePath, 'log')
+            if os.path.exists(logFilePath):
+                return os.path.join(logFilePath, logFileName)
+            else:  # Create the log directory
+                os.mkdir(logFilePath)
+                return os.path.join(logFilePath, logFileName)
+
+    def write(self, message):
+        if self.verbose:
+            self.terminal.write(message)
+            self.terminal.flush()
+        self.log.write(message)
+
+    def writeLog(self, message, lineNumber, severity='normal', data=None):
+        """
+        Function that writes out to the log file and console based on verbose.
+        The function will change behavior slightly based on severity of the message.
+
+        :param message: str: Message to write
+        :param lineNumber: int: File line number that created this log entry.
+        :param severity: str: Defines what the message is related to. Is the message:
+                    - [N] : A 'normal' notification
+                    - [W] : A 'warning'
+                    - [E] : An 'error'
+                    - [!] : A 'code-breaker error' (errors that are followed by the script exitting)
+        :param data: dict: A dictionary that will contain additional information when a code-breaker error occurs
+                Attributes:
+                    - code : error code
+                        1 : Generic error, only print the message.
+                        2 : An API call was not successful. Response object attached.
+                        3 : YAML loading error. Error object attached
+                    - response : str
+                        JSON-like str - the response recieved from the request in conern at the point of error.
+                    - error : str
+                        String produced by exception if an exception occured
+        """
+        # Get a timestamp
+        timestamp = self.getCurrentTimestamp()
+
+        # Format the message based on severity
+        lineNumber = str(lineNumber)
+        if severity == 'normal':
+            indicator = '[N]'
+            toWrite = ' ' + indicator + '  |  ' + lineNumber + '  | ' + timestamp + ': ' + message
+        elif severity == 'warning':
+            indicator = '[W]'
+            toWrite = ' ' + indicator + '  |  ' + lineNumber + '  | ' + timestamp + ': ' + message
+        elif severity == 'error':
+            indicator = '[X]'
+            toWrite = ' ' + indicator + '  |  ' + lineNumber + '  | ' + timestamp + ': ' + message
+        elif severity == 'code-breaker':
+            indicator = '[!]'
+            toWrite = ' ' + indicator + '  |  ' + lineNumber + '  | ' + timestamp + ': ' + message
+
+            if data['code'] == 2:  # Response recieved but unsuccessful
+                details = '\n[ErrorDetailsStart]\n' + data['response'] + '\n[ErrorDetailsEnd]'
+                toWrite = toWrite + details
+            elif data['code'] == 3:  # YAML loading error
+                details = '\n[ErrorDetailsStart]\n' + data['error'] + '\n[ErrorDetailsEnd]'
+                toWrite = toWrite + details
+
+        # Write out the message
+        self.log.write(toWrite + '\n')
+        if self.verbose:
+            self.terminal.write(message + '\n')
+            self.terminal.flush()
+
+    def getCurrentTimestamp(self):
+        """
+        Simple function that calculates the current time stamp and simply formats it as a string and returns.
+        Mainly aimed for logging.
+
+        Returns
+        -------
+            - timestamp : str
+                A formatted string of current time
+        """
+        return datetime.now().strftime("%H:%M:%S.%f")[:-3]
+
+    def exceptionLogger(self, exctype, value, traceBack):
+        """
+        A simple printing function that will take place of the sys.excepthook function and print the results to the log instead of the console.
+
+        Parameters
+        ----------
+            - exctype : object
+                Exception type and details
+            - Value : str
+                The error passed while the exception was raised
+            - traceBack : traceback object
+                Contains information about the stack trace.
+        """
+        LOGGER.write('Exception Occured! Details follow below.\n')
+        LOGGER.write('Type:{}\n'.format(exctype))
+        LOGGER.write('Value:{}\n'.format(value))
+        LOGGER.write('Traceback:\n')
+        for i in traceback.format_list(traceback.extract_tb(traceBack)):
+            LOGGER.write(i)
+
+    def flush(self):
+        # This flush method is needed for python 3 compatibility.
+        # This handles the flush command by doing nothing.
+        # You might want to specify some extra behavior here.
+        pass
+
+
+# Determine log file path
+LOGGER = Logger(verbose=False)
 if __name__ == '__main__':
-    # sys.stdout = LOGGER
-    # sys.excepthook = LOGGER.exceptionLogger
+    sys.stdout = LOGGER
+    sys.excepthook = LOGGER.exceptionLogger
     main(sys.argv[1:])
-
 
 """
 if platform == "linux" or platform == "linux2":
@@ -150,8 +489,8 @@ data['Interchangepartnumber'] = data['Interchangepartnumber'].str.replace('/', '
 data['VendorID'] = '82'
 '''
 
-'''
-# Create (Alter) data frame to add column [LineMasterID]
+# Create (Alter) data frame to add column [LineMaster
+'''ID]
 data['LineMasterID'] = '1089'
 '''
 
